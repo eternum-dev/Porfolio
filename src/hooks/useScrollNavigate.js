ff1f8c345a099567163c2ddafd5e6f1a984ae51f
Alejandro Thon <alejandro.thon.j@gmail.com>
2024-03-07 23:54:16 -0300

    update

--- src/hooks/useScrollNavigate.js
+++ src/hooks/useScrollNavigate.js
@@ -4,16 +4,17 @@ import { useLocation } from "react-router-dom";
 
 
 
-export const useScrollNavigate = (  ) => {
-    const location  = useLocation();
+export const useScrollNavigate = () => {
+    const location = useLocation();
+    
     useEffect(() => {
-
-        const hash = location.hash.slice(1); // Remove the '#' character from the hash
+        const hash = location.hash.slice(1);
 
         if (hash) {
             const element = document.getElementById(hash);
+            
             if (element) {
-                element.scrollIntoView({ behavior: 'smooth' });
+                element.scrollIntoView({ behavior: 'smooth', block: 'start' });
             }
         }
     }, [location]);
