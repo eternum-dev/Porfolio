75684c77161c1c95c9cfdcb26ef6cc23d0c75d9a
Alejandro Thon <alejandro.thon.j@gmail.com>
2024-03-08 18:53:46 -0300

    includes top margin in offset

--- src/hooks/useScrollNavigate.js
+++ src/hooks/useScrollNavigate.js
@@ -6,15 +6,21 @@ import { useLocation } from "react-router-dom";
 
 export const useScrollNavigate = () => {
     const location = useLocation();
-    
+
     useEffect(() => {
         const hash = location.hash.slice(1);
 
         if (hash) {
             const element = document.getElementById(hash);
-            
+
             if (element) {
-                element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                let position = element.getBoundingClientRect();
+                window.scrollTo({
+                    left: position.left,
+                    top: position.top + window.scrollY - 140,
+                    behavior: 'smooth'
+                })
+                // element.scrollIntoView({ behavior: 'smooth', block: 'start' });
             }
         }
     }, [location]);
