22618f0654786c9c958fad6b04a70c73c7a78288
Alejandro Thon <alejandro.thon.j@gmail.com>
2024-03-11 21:37:33 -0300

    create useIntersectionurlupdater

---
+++ src/hooks/useIntersectionUrlUpdater.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+
+
+export const useIntersectionUrlUpdater = (componentRef) => {
+
+    const location = useLocation();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            entries => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                       
+                        const newUrl = `${location.pathname}#${componentRef.current.id}`;
+                        window.history.pushState(null, '', newUrl);
+                    }
+                });
+            },
+            { threshold: 0.5 }
+        );
+        observer.observe(componentRef.current);
+
+        return () => {
+            observer.disconnect();
+        };
+    }, [componentRef]);
+
+    return {
+
+    }
+}
