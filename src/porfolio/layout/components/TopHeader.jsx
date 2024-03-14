3cf5676e8477bbeb6977bd61b5e674bc19284750
eternum-dev <alejandro.thon.j@gmail.com>
2024-03-14 18:40:58 -0300

    create animation when scrolling the page

--- src/porfolio/layout/components/TopHeader.jsx
+++ src/porfolio/layout/components/TopHeader.jsx
@@ -1,5 +1,5 @@
 import { Link, NavLink, useLocation } from 'react-router-dom';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ContactButton } from '../../components/ContactButton';
 import { useScrollNavigate } from '../../../hooks/useScrollNavigate';
 import './topheader.css';
@@ -11,44 +11,58 @@ export const TopHeader = () => {
 
     const location = useLocation();
 
+    const [scrollTop, setScrollTop] = useState(0);
+
+    const haderRef = useRef();
+
     const navRef = useRef();
     const openMenuRef = useRef();
     const closeMenuRef = useRef();
 
     const showNav = () => {
         navRef.current.classList.toggle('navBar');
-
         openMenuRef.current.classList.toggle('disable');
         closeMenuRef.current.classList.toggle('disable');
     }
 
     useScrollNavigate();
 
-
-    // const scrollEvent = () => {
-    //     // console.log(window.scrollY);
-    // }
-    // window.addEventListener('scroll', scrollEvent);
-
     const functi = (proyects) => {
         const hash = location.hash.slice(1);
         if (hash === proyects) {
-            return 'isActive'
+            return 'isActive';
         }
-        return ''
+        return '';
     }
 
     const proyects = 'proyect';
 
 
-    const scrollEvent = () => {
-        // console.log(window.scrollY);
-    }
-    window.addEventListener('scroll', scrollEvent);
+    useEffect(() => {
+        window.addEventListener('scroll', isSticky);
+        return () => {
+            window.removeEventListener('scroll', isSticky);
+        };
+    });
+
+
+    const isSticky = () => {
+        console.log(scrollTop, window.scrollY);
+        if (scrollTop < window.scrollY) {
+            haderRef.current.classList.add('is-sticky');
+            setScrollTop(window.scrollY);
+        } else {
+            haderRef.current.classList.remove('is-sticky');
+            setScrollTop(window.scrollY);
+        }
+    };
+
+
 
     return (
         <header
             className='header'
+            ref={haderRef}
         >
             <Link className='header__link' to="/#home">
                 <img src="src\assets\logo.png" alt="logo" />
@@ -68,13 +82,13 @@ export const TopHeader = () => {
                     </NavLink>
                     <NavLink
                         to='/#skills'
-                        className={({ isActive }) => `navlink ${isActive ? 'isActive' : ''}`}
+                        className={() => `navlink ${functi('skills')}`}
                     >
                         skills
                     </NavLink>
                     <NavLink
                         to='/#about'
-                        className={({ isActive }) => `navlink ${isActive ? 'isActive' : ''}`}
+                        className={() => `navlink ${functi('about')}`}
                     >
                         about me
                     </NavLink>
