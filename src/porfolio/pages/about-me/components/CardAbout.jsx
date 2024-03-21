348a56448348e24d820944856ff1c1e4d24d08cb
eternum-dev <alejandro.thon.j@gmail.com>
2024-03-20 23:58:47 -0300

    update

--- src/porfolio/pages/about-me/components/CardAbout.jsx
+++ src/porfolio/pages/about-me/components/CardAbout.jsx
@@ -1,36 +1,35 @@
-
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import './cardAbout.css';
-import { useRef } from 'react';
+
 
 
 export const CardAbout = ({ data }) => {
-    const refContainer = useRef();
 
-    const showCerticate = () => {
-        refContainer.current.classList.toggle('active');
+    const [showData, setShowData] = useState(false);
+
+    const handleShowData = () => {
+        setShowData((prev) => !prev)
     }
 
+
     return (
-        <article className="about__article"  >
-            <div className="about__content">
+        <article className="about__article" style={{height: !showData && '100%'}} >
+            <div className="about__content" >
                 <h3 className='about__subtitle'>{data.subTitle}</h3>
-                <p>{data.descrription}</p>
+                <button
+                    className='about__button'
+                    onClick={handleShowData}>
+                    <img src="#" alt={!showData ? 'abrir' : 'cerrar'} />
+                </button>
+                {
+                    !showData && <p>{data.descrription}</p>
+                }
             </div>
-            <button
-                className='about__button'
-                onClick={showCerticate}>
-                <img src="#" alt="abrir" />
-            </button>
-
-            <div className='about__conteiner ' ref={refContainer}>
-                <img src={`${data.img}`} alt="certificate" />
-            </div>
-
         </article>
     )
 }
 
 CardAbout.propTypes = {
     data: PropTypes.any
-}
+} 
