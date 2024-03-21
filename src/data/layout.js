00d5293160d338ac15ee50b3109d98d975be584a
eternum-dev <alejandro.thon.j@gmail.com>
2024-03-21 18:11:11 -0300

    create data  for layout component

---
+++ src/data/layout.js
@@ -0,0 +1,45 @@
+
+
+export const header = {
+    link: 'home',
+    navLink: [
+        {
+            to: 'proyect',
+            name: 'Proyect'
+        },
+        {
+            to: 'skills',
+            name: 'Skills'
+        },
+        {
+            to: 'about',
+            name: 'About Me'
+        }],
+    button: 'contact'
+}
+export const footer = {
+    link: 'home',
+    navLink: [
+        {
+            to: 'proyect',
+            name: 'Proyect'
+        },
+        {
+            to: 'skills',
+            name: 'Skills'
+        },
+        {
+            to: 'about',
+            name: 'About Me'
+        }],
+    network: [{
+        src: ' src/assets/icons/GitHub.svg',
+        name: 'GitHub',
+        to: 'github'
+    },
+    {
+        src: 'src/assets/icons/LinkedIn.svg',
+        name: 'Linkedin',
+        to: ' linkedin'
+    },]
+}
