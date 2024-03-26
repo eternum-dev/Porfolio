f092d6fd177cc3b58ad5d34f5e44e2529d0b677d
eternum-dev <alejandro.thon.j@gmail.com>
2024-03-26 17:35:16 -0300

    create component for replace  svg and delete svg

---
+++ src/assets/icons/Email.jsx
@@ -0,0 +1,34 @@
+
+function Email() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="none"
+            strokeWidth="1.5"
+            color="#856b47"
+            viewBox="0 0 24 24"
+        >
+            <path
+                stroke="#856b47"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5 9l4.5 3L14 9"
+            ></path>
+            <path
+                stroke="#856b47"
+                strokeLinecap="round"
+                d="M17 19H3a2 2 0 01-2-2V7a2 2 0 012-2h13a2 2 0 012 2v2"
+            ></path>
+            <path
+                stroke="#856b47"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M23 14h-6m0 0l3-3m-3 3l3 3"
+            ></path>
+        </svg>
+    );
+}
+
+export default Email;
