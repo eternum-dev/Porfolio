3f32bd1e6f4d11b19c4cc39f09a9ff341aab2a13
eternum-dev <alejandro.thon.j@gmail.com>
2024-03-26 23:28:25 -0300

    add new section in te my other  proyects

--- src/porfolio/pages/proyect/components/GridProyect.jsx
+++ src/porfolio/pages/proyect/components/GridProyect.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CardProyect } from './CardProyect';
 import './gridProyect.css';
 
@@ -8,16 +9,40 @@ let i = 0;
 
 export const GridProyect = ({ allProyect }) => {
 
+    const [isVisible, setIsVisible] = useState(false);
+
+
     return (
+        <>
+            <h2>work</h2>
+            <section className="proyect__grid">
+                {
+                    allProyect.map(proyect => (
+                        <CardProyect key={i++} proyect={proyect} />
+                    ))
+                }
+            </section>
 
-        <section className="proyect__grid">
-            {
-                allProyect.map(proyect => (
-                    <CardProyect key={i++} proyect={proyect} />
-                ))
-            }
-        </section>
+            <h2>Practicas, ejercicios, otros</h2>
+            <div>
+                <button
+                    onClick={() => setIsVisible(prev => !prev)}
+                    style={{padding:'1rem 1.5rem', borderRadius:'.4rem', border:'1px solid #ccc', width:'100px'}}
+                >
+                    {!isVisible ? 'abrir' : 'cerrar'}
+                </button>
+                <div className="proyect__grid">
+                    {
+                        isVisible && (
+                            allProyect.map(proyect => (
+                                <CardProyect key={i++} proyect={proyect} />
+                            ))
+                        )
+                    }
+                </div>
 
+            </div>
+        </>
 
     )
 }
