5b003b4d618591e50bbff557793ba572c6831532
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-01 23:26:25 -0300

    create the <card/> component  with their styles

---
+++ src/porfolio/components/Card.jsx
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types';
+import './card.css';
+import { Link } from 'react-router-dom';
+
+
+
+export const Card = ({ data }) => {
+
+    return (
+        <Link to={data.repository} >
+            <article className='cardProyect' >
+                <img src={data.img} alt={data.name} className="proyect__img" />
+
+                <div className='cardProyect__info'>
+                    <header className='cardProyect__header'>
+                        <h3>{data.name}</h3>
+                    </header>
+                    {
+                        data.description &&
+                        <p className="proyect__description" >
+                            {data.description}
+                        </p>
+                    }
+                </div>
+
+                {
+                    data.tecnologias &&
+                    <div className='tec__container'>
+                        {
+                            data.tecnologias &&
+                            data.tecnologias.map(tec => (
+                                <img src={`src/assets/stack-tech-icon/${tec.name}.svg`}
+                                    alt={tec.name}
+                                    className='tec__img'
+                                    key={tec.name}
+                                />
+                            ))
+                        }
+                    </div>
+                }
+            </article>
+        </Link>
+
+    )
+}
+
+
+
+
+Card.propTypes = {
+    data: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        img: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        tecnologias: PropTypes.arrayOf(
+            PropTypes.shape({
+                name: PropTypes.string.isRequired
+            })
+        ),
+        repository: PropTypes.string.isRequired
+    }).isRequired,
+};
