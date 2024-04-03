747789b8bb31fbafe54f652ecdde831f559ead44
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-02 23:11:20 -0300

    implement tooltip

--- src/porfolio/pages/skills/components/CardSkill.jsx
+++ src/porfolio/pages/skills/components/CardSkill.jsx
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
 import './cardSkill.css';
+import { Tooltip } from '../../../components/Tooltip';
 
 export const CardSkill = ({ technology }) => {
     return (
         <article key={technology.name} className='cardSkill'>
-            <header className='cardSkill__header'>
-                <h3 className='cardSkill__title'>{technology.name}</h3>
-            </header>
+            <Tooltip title={technology.name}>
+                <header className='cardSkill__header'>
+                    <h3 className='cardSkill__title'>{technology.name}</h3>
+                </header>
+            </Tooltip>
+
             <img className='cardSkill__img' src={technology.icon} alt={technology.name} />
         </article>
     )
