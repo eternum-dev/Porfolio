6565514672665d9f40cc67911e648df0585e4f8a
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-06 11:56:46 -0300

    Updates

---
+++ assets/icons/Cv.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+export const Cv = ({ size = '32' })=> {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={size}
+            height={size}
+            fill="none"
+            strokeWidth="1.5"
+            color="#856b47"
+            viewBox="0 0 24 24"
+        >
+            <path
+                stroke="#856b47"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 6h12M6 10h12M13 14h5M13 18h5M2 21.4V2.6a.6.6 0 01.6-.6h18.8a.6.6 0 01.6.6v18.8a.6.6 0 01-.6.6H2.6a.6.6 0 01-.6-.6z"
+            ></path>
+            <path
+                stroke="#856b47"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18v-4h3v4H6z"
+            ></path>
+        </svg>
+    );
+}
+
+export default Cv;
+
+
+Cv.propTypes = {
+    size: PropTypes.string
+}
