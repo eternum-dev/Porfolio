527ee7325834f343709eb2e3967f62ee718a5d6f
eternum-dev <alejandro.thon.j@gmail.com>
2024-04-06 16:20:58 -0300

    create the helper and separate the logic of sending emails

---
+++ src/helpers/sendEmail.js
@@ -0,0 +1,23 @@
+import emailjs from '@emailjs/browser';
+
+
+
+const myName = 'Alejandro'
+
+
+export const sendEmail = (formState) => {
+    emailjs
+        .send('service_zrxli9i',
+            'template_pohcepq',
+            { to_name: myName, ...formState },
+            { publicKey: 'cvGSDhlMofn5MHcZK', })
+        .then(
+            () => {
+                console.log('SUCCESS!');
+            },
+            (error) => {
+                console.log(formState);
+                console.log('FAILED...', error.text);
+            },
+        );
+}
